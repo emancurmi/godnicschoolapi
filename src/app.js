009113cb2bb956fa8328a9c9e3734062df696072
Eman Curmi <emancurmi@gmail.com>
2021-08-01 05:05:44 -0400

    cors removed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const helmet = require('helmet')
 const { ADDRESS, PORT, NODE_ENV } = require('./config')
 const validateBearerToken = require('./validate-bearer-token')
@@ -11,29 +10,9 @@ const app = express()
 const helpRouter = require('./help/help-router')
 const bookingRouter = require('./booking/booking-router')
 
-let whitelist = [
-    'http://localhost:8000',
-    'http://localhost:3000',
-    'https://schoolbookingapp.godnicgarage.com',
-    'https://schoolbookingapi.godnicgarage.com',
-    'https://godnicschoolapp.vercel.app']
-
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test'
 }))
-
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        if (!origin) return callback(null, true);
-        if (whitelist.indexOf(origin) === -1) {
-            var message = `The CORS policy for this origin doesn't ` +
-                `allow access from the particular origin.`;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
 
 app.use(helmet())
 app.use(validateBearerToken)
